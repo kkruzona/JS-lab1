3a8e2b312edcad5168ba55a31839197567d5214e
Kelsey Kruzona <kelsey.herlein@gmail.com>
2021-03-24 08:34:35 -0400

    ready to commit

---
+++ lab1.js
@@ -0,0 +1,27 @@
+var name = 'Mitch Cuckovich';
+var age = 25;
+var birthday = 'January 24';
+let detroitGC = true;
+const lifeEvents = ["I was born in Troy, Michigan.", "I went to Hope College", "I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.", "I'm a graduate of the front-end bootcamp."];
+
+if (detroitGC === true) {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
+} else {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
+}
+
+for (let i = 0;  i < lifeEvents.length; i++) {
+    console.log(lifeEvents[i]);
+}
+
+let counter = 0;
+while (true){
+    let randomNumber = Math.floor(Math.random() * (10 - 1)) + 1;
+
+if (randomNumber !== 5) {
+    counter++;
+    console.log(`${randomNumber} !== 5`)
+} else {
+    console.log(`5 === 5. It took ${counter} iterations to randomly generate the number 5.`)
+    break;
+}}
